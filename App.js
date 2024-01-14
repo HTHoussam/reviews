ff506a781f1004c0d923a15a49b7d77ac1f83e3a
Houssam Habbat <houssamhabbat0@gmail.com>
2024-01-14 12:09:44 +0000

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useCallback, useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [title, setTitle] = useState("my title");
+
+  const handleTextChange = useCallback(
+    (enteredText) => {
+      setTitle(enteredText);
+    },
+    [setTitle]
+  );
+  const handlePressButton = useCallback(() => {
+    console.log(title);
+  }, [title]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Enter you notes {title}</Text>
+        <TextInput
+          onChangeText={handleTextChange}
+          placeholder="Note"
+          style={styles.input}
+        />
+      </View>
+      <Button onPress={handlePressButton} title="button" color={"red"} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    padding: 30,
+  },
+  inputContainer: {
+    paddingTop: 50,
+    alignItems: "center",
+    textAlign: "center",
+    gap: 4,
+    borderBottomColor: "black",
+  },
+  input: {
+    marginBottom: 10,
+    fontSize: 14,
+    borderRadius: 8,
   },
 });
