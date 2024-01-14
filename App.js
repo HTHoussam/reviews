66ac3fcd333e4e4f7b70e4b08066a5fef27e35c2
Houssam Habbat <houssamhabbat0@gmail.com>
2024-01-14 13:09:36 +0000

    feat: add delete functionnality

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { useCallback, useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
-  const [title, setTitle] = useState("my title");
+  const [title, setTitle] = useState("");
+  const [notesList, setNotesList] = useState([]);
 
   const handleTextChange = useCallback(
     (enteredText) => {
@@ -12,19 +22,38 @@ export default function App() {
     [setTitle]
   );
   const handlePressButton = useCallback(() => {
-    console.log(title);
+    if (title.length <= 0) return;
+    setNotesList((prev) => [...prev, title]);
+    setTitle("");
   }, [title]);
+  const handleDeleteItem = useCallback((itemIndex) => {
+    setNotesList((prev) => prev.filter((item, index) => index !== itemIndex));
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <Text>Enter you notes {title}</Text>
         <TextInput
+          value={title}
           onChangeText={handleTextChange}
           placeholder="Note"
           style={styles.input}
         />
       </View>
       <Button onPress={handlePressButton} title="button" color={"red"} />
+      <ScrollView>
+        <FlatList
+          data={notesList}
+          renderItem={({ item, index }) => (
+            <View style={styles.listItemWrapper} key={item}>
+              <Text style={styles.listText}>{item}</Text>
+              <Pressable onPress={() => handleDeleteItem(index)}>
+                <View style={styles.pressableIconWrapper}></View>
+              </Pressable>
+            </View>
+          )}
+        />
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -47,5 +76,21 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     fontSize: 14,
     borderRadius: 8,
+  },
+  listText: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  listItemWrapper: {
+    padding: 5,
+    marginTop: 4,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  pressableIconWrapper: {
+    backgroundColor: "red",
+    width: 30,
+    height: 30,
   },
 });
